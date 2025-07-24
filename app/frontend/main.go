04d837d04077bb795e6f52b18b62ad1121aa99b5
tetsuya kikuchi <tkikuchi07f@gmail.com>
2025-07-24 11:19:08 +0000

    feat: hands-on step2 Taiwan

--- app/frontend/main.go
+++ app/frontend/main.go
@@ -19,7 +19,7 @@ type Response struct {
 
 func main() {
 	http.HandleFunc("/frontend", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello first handson edited:D\n")
+		fmt.Fprintf(w, "Hello first handson edited Taiwan:D\n")
 	})
 
 	http.HandleFunc("/random", func(w http.ResponseWriter, r *http.Request) {
